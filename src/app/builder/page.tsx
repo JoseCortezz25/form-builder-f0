30791287ba541fa4382fa0615b7637fed0afd508
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-02-23 16:40:13 -0500

    Add header component and update layout structure; enhance global styles

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <main>
+      <div className="mx-auto max-w-[800px]">
+        <section className="w-full flex justify-between border border-red-300">
+          <div className="w-full">
+
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-6 py-4 shadow-sm">
+      <nav>asdsasa</nav>
+    </header>
+  );
+};
